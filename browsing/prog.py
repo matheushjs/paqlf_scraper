774560c59551e9d90cac228351abe19e9a33268d
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-20 18:26:41 -0300

    Successfully get cookies

--- browsing/prog.py
+++ browsing/prog.py
@@ -1,5 +1,6 @@
 #Used to make requests
-import urllib.request
+from urllib.request import urlopen, build_opener, install_opener, Request, OpenerDirector, HTTPCookieProcessor
+from urllib.parse import urlencode
 import os
 
 login_url = "https://secure.runescape.com/m=weblogin/loginform.ws?mod=www&ssl=0&dest=community%3Fset_lang%3D0"
@@ -14,17 +15,24 @@ values = {'login-username': username,
 headers = {}
 headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
 
-data = urllib.parse.urlencode(values)
+data = urlencode(values)
 data = data.encode('utf-8')
-request = urllib.request.Request(login_url, data, headers=headers)
+
+cookie_proc = HTTPCookieProcessor()
+opener = build_opener(cookie_proc)
+install_opener(opener)
+
+request = Request(login_url, data, headers=headers)
 
 # We need to save cookies.
 # On next request, we must send the cookie back to the server.
 
-response = urllib.request.urlopen(request)
+response = urlopen(request)
 
 #print(urllib.request.urlopen(fetch_url).read())
 #print(response.read())
 
-open("output.html", 'w').write( response.read().decode('utf-8') )
-os.system('google-chrome output.html')
+print(response.getcode())
+print(response.info())
+print(cookie_proc.cookiejar.make_cookies(response, request))
+#print(response.read().decode('utf-8'))
