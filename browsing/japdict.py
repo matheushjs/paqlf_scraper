4e27d1f9cc692c7a6f050c95f3a3e5e0c3c78794
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-20 19:31:36 -0300

    Successfully grab data on romaji word

---
+++ browsing/japdict.py
@@ -0,0 +1,27 @@
+from urllib.request import *
+from urllib.parse import *
+
+lookup = input("Romaji word to lookup: ")
+lookup = 'chotto'
+
+url1 = "http://nihongo.monash.edu/cgi-bin/wwwjdic?1E"
+
+values = {
+    'dsrchkey': lookup,
+    'dicsel': 1,
+    'dsrchtype': 'J'}
+
+headers = {}
+headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+
+data = urlencode(values)
+data = data.encode('utf-8')
+
+request = Request(url1, data, headers=headers)
+
+with urlopen(request) as response:
+
+    print(response.getcode())
+    print(response.info())
+    print(response.geturl())
+    print(response.read().decode('utf-8'))
