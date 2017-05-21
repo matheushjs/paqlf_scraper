fb62f2f916327a34588bfb39fb9cae1acba21d9b
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-20 22:16:16 -0300

    Improve regmatch

--- browsing/regmatch.py
+++ browsing/regmatch.py
@@ -3,8 +3,11 @@ with open('input.html') as fp:
 
 import re
 
-test = 'http://services.runescape.com/m=forum/a=13/c=GNgDP8ssBF0/forums.ws?jptg=ia&jptv=navbar'
-patt = re.compile(r'http://services\.runescape\.com/m=forum/a=13/c=[^/]*/forums\.ws\?jptg=ia\&jptv=navbar')
-mat = re.match(patt, test)
+test = """akjdbwyawjydbajwdawy
+http://services.runescape.com/m=forum/a=13/c=GNgDP8ssBF0/forums.ws?jptg=ia&amp;jptv=navbar"""
+patt = re.compile(r'http://services\.runescape\.com/m=forum/a=13/c=[^/]*/forums\.ws\?jptg=ia\&amp;jptv=navbar')
 
-print(test[mat.start():mat.end()])
+mat = re.findall(patt, full)
+
+print(mat)
+#print(full[mat.start():mat.end()])
